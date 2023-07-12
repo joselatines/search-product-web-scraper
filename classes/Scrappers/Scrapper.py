8652207e34ff5f7bac9647d5a8e0b450322273b2
joselatines <joselatines33@gmail.com>
2023-07-12 11:04:03 -0400

    scrapper

--- classes/Scrappers/Scrapper.py
+++ classes/Scrappers/Scrapper.py
@@ -1,38 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
-
 from classes.Products.Product import Product
 
 
 class Scrapper:
-    soup: BeautifulSoup = None
-
     def __init__(self, website_url, headers={}):
         self.website_url = website_url
         self.headers = headers
+        self.soup = None
 
-    # get base website without managing the data yet
     def request_website(self, product: str) -> BeautifulSoup:
         response = requests.get(f"{self.website_url}{product}", headers=self.headers)
-
         self.soup = BeautifulSoup(response.text, "html.parser")
 
         return self.soup
 
-    def scrape_products_testing(self) -> Product:
-        html_elements = {
-            "parent": {"tag": "div", "class_name": "s-result-list"},
-            "title": {
-                "tag": "span",
-                "class_name": "a-size-medium a-color-base a-text-normal",
-            },
-            "price": {"tag": "span", "class_name": "a-price-whole"},
-            "link": {
-                "tag": "a",
-                "class_name": "a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal",
-            },
-        }
-
+    def scrape_products_testing(self, html_elements) -> Product:
         parent = html_elements["parent"]
         title = html_elements["title"]
         price = html_elements["price"]
@@ -41,12 +24,12 @@ class Scrapper:
         products_html = self.soup.find_all(parent["tag"], class_=parent["class_name"])
 
         def find_html_elements(item):
-            title = item.find(title["tag"], class_=title["class_name"])
-            price = item.find(price["tag"], class_=price["class_name"])
-            link = item.find(link["tag"], class_=link["class_name"]).get("href")
+            title_elem = item.find(title["tag"], class_=title["class_name"])
+            price_elem = item.find(price["tag"], class_=price["class_name"])
+            link_elem = item.find(link["tag"], class_=link["class_name"]).get("href")
 
-            return {title, price, link}
+            return Product(title_elem, price_elem, link_elem)
 
-        html_element = list(map(find_html_elements, products_html))
+        html_elements = list(map(find_html_elements, products_html))
 
-        return html_element
+        return html_elements
